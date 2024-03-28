97aadc6457bc7683697eafe0c42ef52e5472cbc4
danielugulino <d.ugulino@picalike.com>
2024-03-28 17:48:27 -0300

    feat: updated prototype control

--- car_control/car_control.ino
+++ car_control/car_control.ino
@@ -1,17 +1,11 @@
-// This example code is in the Public Domain (or CC0 licensed, at your option.)
-// By Evandro Copercini - 2018
-//
-// This example creates a bridge between Serial and Classical Bluetooth (SPP)
-// and also demonstrate that SerialBT have the same functionalities of a normal Serial
-
 #include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
-#define VELOCIDADE_DO_SOM 0.034 // definir a velocidade do som em cm/uS
-#define CM_PARA_POLEGADA 0.393701
-#define DIST_LIM 40
+#define SOUND_SPEED 0.034
+#define DIST_LIM 50
+
 BluetoothSerial SerialBT;
 
 const int pin_ena = 4;
@@ -30,13 +24,10 @@ float speedMb = 0;
 
 char last_control = 'N';
 
-long duracao;
-float distancCm;
-
 void setup()
 {
   Serial.begin(9600);
-  SerialBT.begin("ESP32test"); // Bluetooth device name
+  SerialBT.begin("ESP32test");
   Serial.println("The device started, now you can pair it with bluetooth!");
 
   pinMode(pin_ena, OUTPUT);
@@ -47,68 +38,68 @@ void setup()
   pinMode(pin_in2, OUTPUT);
   pinMode(pin_in4, OUTPUT);
 
-  pinMode(trigPin, OUTPUT); // Define o trigPin como uma saída
-  pinMode(echoPin, INPUT);  // Define o echoPin como uma entrada
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop()
 {
   char control = 'N';
-  float distancia = distance_sensor();
-  Serial.println(last_control);
+  float distance = distance_sensor();
+
   if (SerialBT.available())
   {
     control = SerialBT.read();
   }
 
-  if (distancia >= DIST_LIM)
+  if (distance >= DIST_LIM)
   {
-    switch (control)
+    switch ((char)control)
     {
     case '1':
-      startEngine();
-      controleVelocidade(15, 15);
+      startEngine(3);
+      speedControl(14.5, 15);
       break;
     case '2':
-      startEngine();
-      controleVelocidade(16.5, 17);
+      startEngine(3);
+      speedControl(16.5, 17);
       break;
     case '3':
-      startEngine();
-      controleVelocidade(18, 19);
+      startEngine(3);
+      speedControl(18.5, 19);
       break;
     case '4':
-      startEngine();
-      controleVelocidade(20.5, 22);
+      startEngine(3);
+      speedControl(20.5, 22);
       break;
     case '5':
-      startEngine();
-      controleVelocidade(25, 25);
+      startEngine(3);
+      speedControl(25, 25);
       break;
-    case 'P':
+    case 'S':
       stop();
       break;
-    case 'D':
-      controleDirecao(1);
+    case 'R':
+      directionControl(1);
       break;
     case 'L':
-      controleDirecao(2);
+      directionControl(2);
       break;
     }
   }
-  else if (distancia <= DIST_LIM)
+  else if (distance <= DIST_LIM)
   {
     stop();
-    if (last_control != 'N' && last_control != 'P')
+    if (last_control != 'N' && last_control != 'S')
     {
-      while (distancia <= DIST_LIM)
+      while (distance <= DIST_LIM)
       {
         delay(500);
         control = (char)SerialBT.read();
         if(SerialBT.read() != -1){
           break;
         }
-        else if (control == 'P')
+        else if (control == 'S')
         {
           stop();
           break;
@@ -116,11 +107,11 @@ void loop()
         else
         {
           rotate();
-          distancia = distance_sensor();
-          if (distancia >= DIST_LIM)
+          distance = distance_sensor();
+          if (distance >= DIST_LIM)
           {
-            startEngine();
-            controleVelocidade(speedMa, speedMb);
+            startEngine(3);
+            speedControl(speedMa, speedMb);
             break;
           }
         }
@@ -128,43 +119,57 @@ void loop()
     }
   }
 
-  if (SerialBT.read() != -1 && control != 'N')
+  if (control != 'N')
   {
     last_control = (char)control;
   }
 }
 
 float distance_sensor()
-{ // Limpa o trigPin
-
+{ 
+  long length;
+  float distanceCM;
   digitalWrite(trigPin, LOW);
-  delayMicroseconds(2); // Define o trigPin no estado ALTO por 10 microssegundos
+  delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Lê o echoPin, retorna o tempo de viagem da onda sonora em microssegundos
-  duracao = pulseIn(echoPin, HIGH);
-  // Calcule a distância
-  distancCm = duracao * VELOCIDADE_DO_SOM / 2; // Converter para centimetros
+  length = pulseIn(echoPin, HIGH);
+  distanceCM = length * SOUND_SPEED / 2;
   delay(200);
-  return distancCm;
+  return distanceCM;
 }
 
-void startEngine()
+void startEngine(int engine)
 {
-  analogWrite(pin_enb, 50);
-  analogWrite(pin_ena, 50);
-  digitalWrite(pin_in1, HIGH);
-  digitalWrite(pin_in2, LOW);
-  digitalWrite(pin_in3, HIGH);
-  digitalWrite(pin_in4, LOW);
+  if(engine == 1){
+    analogWrite(pin_ena, 45);
+    digitalWrite(pin_in1, HIGH);
+    digitalWrite(pin_in2, LOW);
+    digitalWrite(pin_in3, LOW);
+    digitalWrite(pin_in4, LOW);
+  }
+  if(engine == 2){
+    analogWrite(pin_enb, 50);
+    digitalWrite(pin_in1, LOW);
+    digitalWrite(pin_in2, LOW);
+    digitalWrite(pin_in3, HIGH);
+    digitalWrite(pin_in4, LOW);
+  }
+  else if (engine == 3){
+    analogWrite(pin_ena, 45);
+    analogWrite(pin_enb, 50);
+    digitalWrite(pin_in1, HIGH);
+    digitalWrite(pin_in2, LOW);
+    digitalWrite(pin_in3, HIGH);
+    digitalWrite(pin_in4, LOW);
+  }
   delay(50);
 }
 
-void controleVelocidade(float Ma, float Mb)
+void speedControl(float Ma, float Mb)
 {
-  Serial.println("Moving Forward");
   speedMa = Ma;
   speedMb = Mb;
   analogWrite(pin_enb, speedMb);
@@ -175,27 +180,28 @@ void controleVelocidade(float Ma, float Mb)
   digitalWrite(pin_in4, LOW);
 }
 
-void controleDirecao(int direcao)
+void directionControl(int direcao)
 {
-  startEngine();
-  analogWrite(pin_ena, 16.5);
-  analogWrite(pin_enb, 17);
-  if (direcao == 1)
+  if (direcao == 1) // right
   {
+    startEngine(1);
+    analogWrite(pin_ena, 16.5);
     digitalWrite(pin_in1, HIGH);
     digitalWrite(pin_in2, LOW);
     digitalWrite(pin_in3, LOW);
     digitalWrite(pin_in4, LOW);
   }
-  if (direcao == 2)
+  if (direcao == 2) // left
   {
+    startEngine(2);
+    analogWrite(pin_enb, 17);
     digitalWrite(pin_in1, LOW);
     digitalWrite(pin_in2, LOW);
     digitalWrite(pin_in3, HIGH);
     digitalWrite(pin_in4, LOW);
   }
   delay(1000);
-  startEngine();
+  startEngine(3);
   analogWrite(pin_ena, speedMa);
   analogWrite(pin_enb, speedMb);
   digitalWrite(pin_in1, HIGH);
@@ -206,8 +212,6 @@ void controleDirecao(int direcao)
 
 void stop()
 {
-  // Stop the DC motor
-  Serial.println("Motor stopped");
   digitalWrite(pin_in1, LOW);
   digitalWrite(pin_in2, LOW);
   digitalWrite(pin_in3, LOW);
@@ -216,7 +220,7 @@ void stop()
 
 void rotate()
 {
-  startEngine();
+  startEngine(1);
   digitalWrite(pin_in1, HIGH);
   digitalWrite(pin_in2, LOW);
   digitalWrite(pin_in3, LOW);
